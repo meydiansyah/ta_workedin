cf5267c04512e10b282761615c0e95302bacb858
meydiansyah <meydiansyah79@gmail.com>
2023-01-05 17:40:28 +0700

    update user seeders

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,30 +4,31 @@ namespace Database\Seeders;
 
 use App\Models\Role;
 use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 
 class UserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
 
-        Role::create(['name' => 'Admin']);
-        Role::create(['name' => 'Client']);
-        Role::create(['name' => 'Freelance']);
+		Role::create(['name' => 'Admin']);
+		Role::create(['name' => 'Client']);
+		Role::create(['name' => 'Freelance']);
 
-        User::create([
-            'name'              => 'admin',
-            'email'             => 'admin@admin.com',
-            'password'          => bcrypt('password'),
-            'remember_token'    => null,
-            'role_id'           => 1,
-            'status_id'         => 1,
-			'profile_photo_url' => null,
-        ]);
-    }
+		User::create([
+			'name'              => 'admin',
+			'email'             => 'admin@admin.com',
+			'password'          => bcrypt('password'),
+			'remember_token'    => null,
+			'role_id'           => 1,
+			'status_id'         => 1,
+			'email_verified_at'	=> Carbon::now(),
+		]);
+	}
 }
